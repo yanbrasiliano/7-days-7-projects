1593c7dd3b078739c30573b8dc5b69101743c118
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2022-06-05 00:51:30 -0300

    Update drag and drop project..

--- #7 - Drag-and-Drop-Simulator/script.js
+++ #7 - Drag-and-Drop-Simulator/script.js
@@ -1,18 +1,89 @@
-// Initial Data
+let areas = {
+	a: null,
+	b: null,
+	c: null
+};
 
-
-//Events
-document.querySelector('.item').forEach(item => {
-	item.addEventListener('dragstart',dragStart);
-	item.addEventListener('dragend',dragEnd);
+document.querySelectorAll('.item').forEach(item => {
+	item.addEventListener('dragstart', dragStart);
+	item.addEventListener('dragend', dragEnd);
 });
 
-//Functions
+document.querySelectorAll('.area').forEach(area => {
+	area.addEventListener('dragover', dragOver);
+	area.addEventListener('dragleave', dragLeave);
+	area.addEventListener('drop', drop);
+});
 
-function dragStart(e){
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
 
+// Functions Item
+function dragStart(e) {
+	e.currentTarget.classList.add('dragging');
 }
 
-function dragEnd(e){
-	
+function dragEnd(e) {
+	e.currentTarget.classList.remove('dragging');
+}
+
+// Functions Area
+function dragOver(e) {
+	if (e.currentTarget.querySelector('.item') === null) {
+		e.preventDefault();
+		e.currentTarget.classList.add('hover');
+	}
+}
+
+function dragLeave(e) {
+	e.currentTarget.classList.remove('hover');
+}
+
+function drop(e) {
+	e.currentTarget.classList.remove('hover');
+
+	if (e.currentTarget.querySelector('.item') === null) {
+		let dragItem = document.querySelector('.item.dragging');
+		e.currentTarget.appendChild(dragItem);
+		updateAreas();
+	}
+}
+
+// Functions Neutral Area
+function dragOverNeutral(e) {
+	e.preventDefault();
+	e.currentTarget.classList.add('hover');
+}
+
+function dragLeaveNeutral(e) {
+	e.currentTarget.classList.remove('hover');
+}
+
+function dropNeutral(e) {
+	e.currentTarget.classList.remove('hover');
+	let dragItem = document.querySelector('.item.dragging');
+	e.currentTarget.appendChild(dragItem);
+	updateAreas();
+}
+
+// Logic Functions
+function updateAreas() {
+	document.querySelectorAll('.area').forEach(area => {
+		let name = area.getAttribute('data-name');
+
+		if (area.querySelector('.item') !== null) {
+			areas[name] = area.querySelector('.item').innerHTML;
+		} else {
+			areas[name] = null;
+		}
+	});
+	if (areas.a === '1' && areas.b === '2' && areas.c === '3') {
+		document.querySelector('.areas').classList.add('correct');
+	} else if (areas.a !== null && areas.b !== null && areas.c !== null) {
+		document.querySelector('.areas').classList.add('incorrect');
+	} else {
+		document.querySelector('.areas').classList.remove('correct');
+		document.querySelector('.areas').classList.remove('incorrect');
+	}
 }
